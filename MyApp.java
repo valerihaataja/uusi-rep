a0d5b79aeaf3f44af6aa3e5037a3725b2bc685a5
valerihaataja <valeri.haataja@hotmail.com>
2018-09-26 14:24:43 +0300

    uusi paivitys

--- MyApp.java
+++ MyApp.java
@@ -2,6 +2,7 @@ public class MyApp {
     public static void main(String[] args) { 
 	Product product = new Product("Edam", 3.3, 120);  
     System.out.println("Product value is " + product.countValue());   
+	product.changeAmount(245);
 	product.printProduct();     
 	} 
 } 
@@ -11,13 +12,24 @@ public class MyApp {
  private double price;
  private int amount; //Amount in storage 
  public Product(String name, double price, int amount) { 
- this.name = name;   
- this.price = price;   
- this.amount = amount;  }  
+		this.name = name;   
+		this.price = price;   
+		this.amount = amount;  
+}  
  public double countValue() {   
- return(amount * price);  }  
- public void printProduct() { 
- System.out.printf("Product %s, price %4.1f and balance %d pcs", name,price,amount); 
+		return(amount * price);  
+ }  
+ public void printProduct() 
+{ 
+		System.out.printf("Product %s, price %4.1f and balance %d pcs",
+		name,price,amount); 
  }
+ 
+ public void changeAmount(int newvalue)
+	{
+		amount = newvalue;
+		
+ 
+	}
 
- } 
+} 
